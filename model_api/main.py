409c32930c1d7387ad20282a334bd8e5ce7e8646
Pushpen Tiwari <pushpentiwari2005@gmail.com>
2026-07-29 21:58:01 +0530

    Initial commit

--- model_api/main.py
+++ model_api/main.py
@@ -1,35 +1,35 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-import numpy as np
 import joblib
 import pandas as pd
 
-
 app = FastAPI()
 
-data = joblib.load("Food_Delivery_model.joblib")
+model = joblib.load("Food_Delivery_Model.joblib")
+
 
 class InputData(BaseModel):
-    Order_ID               : int
-    Distance_km            : float
-    Weather                : int
-    Traffic_Level          : int
-    Time_of_Day            : int
-    Vehicle_Type           : int
-    Preparation_Time_min   : float
-    Courier_Experience_yrs : float
+    Order_ID: int
+    Distance_km: float
+    Weather: int
+    Traffic_Level: int
+    Time_of_Day: int
+    Vehicle_Type: int
+    Preparation_Time_min: float
+    Courier_Experience_yrs: float
+
 
 @app.get("/")
-async def read_root():
-    return {"message": "Welcome to the Model API!"} 
+def root():
+    return {"message": "Welcome to the Model API!"}
 
 
-@app.get("/predict")
-async def predict(input_data: InputData):
-    # Convert input data to a DataFrame
-    df = pd.DataFrame([input_data.dict()])
-    
-    # Make prediction
-    prediction = data.predict(df)
-    
-    return {"prediction": prediction[0]}
+@app.post("/predict")
+def predict(input_data: InputData):
+    df = pd.DataFrame([input_data.model_dump()])
+
+    prediction = model.predict(df)
+
+    return {
+        "prediction": float(prediction[0])
+    }
